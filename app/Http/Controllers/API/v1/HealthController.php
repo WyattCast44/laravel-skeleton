a86db38ada65602b99e1ebe9bc992116c3d94b1d
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2022-01-15 01:09:54 -0800

    wip

---
+++ app/Http/Controllers/API/v1/HealthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HealthController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        return response([
+            'version' => 1,
+            'healthy' => true,
+            'timestamp' => now(),
+            'authenticated' => $request->user() ? true : false,
+        ]);
+    }
+}
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,3 +1,6 @@
 <?php
 
-//
+use App\Http\Controllers\API\v1\HealthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('health', HealthController::class);
